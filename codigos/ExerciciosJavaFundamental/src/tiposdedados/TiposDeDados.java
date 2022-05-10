d46a30b23bdc04486a815f54e90f6981d97f974d
UsernameAna90 <username.ana.90@gmail.com>
2022-05-10 19:36:27 -0300

    Adicionado novo codigo de exercicio, tipo de dados.

---
+++ codigos/ExerciciosJavaFundamental/src/tiposdedados/TiposDeDados.java
@@ -0,0 +1,29 @@
+package tiposdedados;
+
+public class TiposDeDados {
+    public static void main(String[] args) {
+        //Criar duas variáveis, para cada tipo de dados
+        char inicialNome = 'A';
+        char inicialNome2 = 'C';
+        String primeiroNome = "Ana";
+        String segundoNome = "Clara";
+        byte idade = 22;
+        byte filhos = 0;
+        short anoNascimento = 2000;
+        short anoAtual = 2022;
+        int id = 8888888;
+        int cep = 9999999;
+        long cpf = 88888888888l;
+        long numeroCel = 99999999999L;
+        float ultimoSalario = 500.50f;
+        float salarioAtual = 1000.50f;
+        double aluguel = 5000000.50d;
+        double alimentacao = 90000000.55d;
+        boolean casada = false;
+        boolean divorciada = false;
+        boolean solteira = true;
+        boolean estudante = true;
+
+        System.out.println("Olá! Meu nome é " + primeiroNome + " " + segundoNome +"!" );
+    }
+}

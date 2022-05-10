e0e3854ba66cb10f7f35db6ba38ea3ca0502b768
UsernameAna90 <username.ana.90@gmail.com>
2022-05-10 12:37:03 -0300

    Criadas classes main e gato, no projeto IntelliJ.

---
+++ codigos/teste-integracao-Git-IDE/IntelliJ/src/primeiroprojeto/PrimeiroProjeto.java
@@ -0,0 +1,11 @@
+package primeiroprojeto;
+
+public class PrimeiroProjeto {
+    public static void main(String[] args) {
+        //INSTANCIANDO OBJETO
+        Gato g1 = new Gato("Faísca", "amarelo", 1);
+
+        //TESTANDO OBJETO
+        System.out.println(g1.toString());
+    }
+}

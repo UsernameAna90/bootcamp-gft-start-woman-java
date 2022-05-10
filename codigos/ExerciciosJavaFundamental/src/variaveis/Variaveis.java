fe95e763a9af4777c313423a23e9d952046b9b5a
UsernameAna90 <username.ana.90@gmail.com>
2022-05-10 17:00:14 -0300

    Adicionado novo código de exercicio

---
+++ codigos/ExerciciosJavaFundamental/src/variaveis/Variaveis.java
@@ -0,0 +1,29 @@
+package variaveis;
+
+public class Variaveis {
+    public static void main(String[] args) {
+        /*
+         * Declarando variáveis de forma correta
+         * Padrão de definição de variáveis:
+         * <visibilidade> <modificador> <tipo> <nome> = <valor>;
+         */
+        String nome = "Ana";
+        int idade = 22;
+        int anoNascimento = 2000;
+        final int NUMERO_CPF = 00011122234;
+
+        /*
+         * Declarando variáveis de forma incorreta
+
+        String 2nome = "Pedro";
+        int id@de = 33;
+        int ano nascimento = 1989;
+
+         */
+
+        //Má praticas de declaração
+        String Nome = "Sabrina";
+        int An0N4sc1m3nt0 = 1989;
+        final int numeroCpf = 00011122235;
+    }
+}
